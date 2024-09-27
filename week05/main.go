cd8f4842079c3c83e96f714ef3d7e26509cd3c6b
juju <202444006@itc.ac.kr>
2024-09-27 12:23:01 +0900

    v0.2

--- week05/main.go
+++ week05/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
-	var i int = 55
+
+	//var i int = 55
+	var f float32 = 4.30 // f:= 4.30
+	i := 55
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+
+	fmt.Printf("%s\n", strings.Title("inha"))
+	fmt.Println(math.Ceil(3.99))
+
 	fmt.Printf("value i: %d\n", i)
 	fmt.Print("value i: ", i, "\n")
 	fmt.Print("value i: ", i)
