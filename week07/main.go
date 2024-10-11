ed6aee45b9cc3aa4f834520b9bba20e283e58135
juju <202444006@itc.ac.kr>
2024-10-11 10:50:21 +0900

    v1.11

--- week07/main.go
+++ week07/main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-
-	var now time.Time = time.Now()
-	var year int = int(now.Year())
-	var month int = int(now.Month())
-	var day int = int(now.Day())
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", year, month, day)
-	fmt.Printf("지금은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
-	fmt.Println(now.Month())
+	var army string = "우리는 ?가와 ?민에 충성을 다하는 대한민? 육군이다"
+	armyFixed := strings.NewReplacer("?", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
 }
